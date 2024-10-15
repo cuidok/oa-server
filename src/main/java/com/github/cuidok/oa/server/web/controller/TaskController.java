4732ad2bbf42f6c7f09ba8660e5a6ec89e5a21e5
cuidok <zapwe@qq.com>
2024-10-15 19:43:54 +0800

    Define the controller

---
+++ src/main/java/com/github/cuidok/oa/server/web/controller/TaskController.java
@@ -0,0 +1,11 @@
+package com.github.cuidok.oa.server.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class TaskController {
+
+}
---
+++ src/main/java/com/github/cuidok/oa/server/web/controller/VerificationController.java
@@ -0,0 +1,7 @@
+package com.github.cuidok.oa.server.web.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VerificationController {
+}
